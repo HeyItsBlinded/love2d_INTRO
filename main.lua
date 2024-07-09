a270b6425bd23c56d1e2933220d967002238782f
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-07-08 22:52:41 -0700

    Moving food to free positions

--- main.lua
+++ main.lua
@@ -17,11 +17,29 @@ function love.load()
         y = love.math.random(1, gridYCount),
     }
 
+    -- moves food to positions not occupied by snake
     function moveFood()
-        foodPosition = {
-            x = love.math.random(1, gridXCount),
-            y = love.math.random(1, gridYCount),
-        }
+        local possibleFoodPositions = {}
+        for foodX = 1, gridXCount do 
+            for foodY = 1, gridYCount do 
+                local possible = true
+
+                for segmentIndex, segment in ipairs(snakeSegments) do 
+                    if foodX == segment.x and foodY == segment.y then
+                        possible = false
+                    end
+                end
+
+                if possible then
+                    table.insert(possibleFoodPositions, {x = foodX, y = foodY})
+                end
+
+            end
+        end
+
+        foodPosition = possibleFoodPositions[
+            love.math.random(#possibleFoodPositions)
+        ]
     end
 
     moveFood()
